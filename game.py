3e89e33959ca137c471a87bfb507ce13eea0187d
zuevanton <wnfgnqpw@gmail.com>
2021-07-01 20:57:55 +0300

    added lesson 15

--- game.py
+++ game.py
@@ -1,46 +1,81 @@
-def update_user_word(secret_word, user_word, char):
-    new_user_word = ''
-    for i in range(len(secret_word)):
-        if secret_word[i] == char:
-            new_user_word += char
+def create_field():
+    field = []
+    for i in range(3):
+        field.append(['*'] * 3)
+    return field
+
+
+def print_field(field):
+    for i in range(3):
+        for j in range(3):
+            print(field[i][j], end='')
+        print()
+
+
+def win(field):
+    for i in range(3):
+        if field[i][0] == field[i][1] == field[i][2] and field[i][0] != '*':
+            return True
+        if field[0][i] == field[1][i] == field[2][i] and field[0][i] != '*':
+            return True
+        if field[0][0] == field[1][1] == field[2][2] and field[0][0] != '*':
+            return True
+        if field[0][2] == field[1][1] == field[2][2] and field[2][0] != '*':
+            return True
+    return False
+
+
+def end_game(field):
+    if win(field):
+        return True
+    for i in range(3):
+        for j in range(3):
+            if field[i][j] == '*':
+                return False
+
+    return True
+
+
+def validate_input(player_input):
+    if player_input.isdigit():
+        player_input = int(player_input)
+        if 1 <= player_input <= 3:
+            return player_input
         else:
-            new_user_word += user_word[i]
-    return new_user_word
-
-
-secret_word = 'академия'
-user_word = '********'
-user_symbols = ''
-attempts_counter = 0
-print('отгадайте слово. где вы проходите обучение?')
-print(user_word)
-
-while user_word != secret_word:
-    print('введите букву')
-    user_char = input()
-    if len(user_char) != 1 or not ord('а') <= ord(user_char.lower()) <= ord('я'):
-        continue
-    if user_char in user_symbols:
-        print('вы уже вводили этот символ')
-        continue
-    user_symbols += user_char
-    new_user_word = update_user_word(secret_word, user_word, user_char)
-    attempts_counter += 1
-    if user_word == new_user_word:
-        print('такой буквы нет в слове')
+            print('введите число от 1 до 3')
+            return validate_input(input())
     else:
-        print('ура! такая буква есть')
-    user_word = new_user_word
-    print(user_word)
+        print('введите число от 1 до 3')
+        return validate_input(input())
 
-    if secret_word == user_word:
-        print(f'ура, слово отгадано за {attempts_counter} попыток. хотите сыграть еще? да/нет')
-        if input().lower() == 'да':
-            secret_word = 'верстки'
-            user_word = '*******'
-            attempts_counter = 0
-            user_symbols = ''
-            print('чему обучаетесь?')
-            print(user_word)
+
+def init_game():
+    field = create_field()
+    current_symbol = 'X'
+
+    while not end_game(field):
+        print_field(field)
+        print('введите номер строки и столбца')
+        row = validate_input(input())
+        column = validate_input(input())
+        if field[row - 1][column - 1] == '*':
+            field[row - 1][column - 1] = current_symbol
+        else:
+            print('тут уже занято, повторите попытку')
             continue
+        if current_symbol == 'X':
+            current_symbol = 'O'
+        else:
+            current_symbol = 'X'
 
+    if current_symbol == 'X':
+        print('ура победил O')
+    else:
+        print('ура победил X')
+
+    print('хотите сыграть еще? y/n')
+    if input().lower() == 'y':
+        init_game()
+
+
+init_game()
