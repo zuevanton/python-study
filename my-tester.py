8fbc6288c4a5c6d2457e5d9e29c7c1df65330aaf
zuevanton <wnfgnqpw@gmail.com>
2021-07-02 01:33:23 +0300

    fix

--- my-tester.py
+++ my-tester.py
@@ -2,6 +2,7 @@ import random
 
 questions = []
 answers = []
+result = []
 
 
 def create_data():
@@ -15,9 +16,7 @@ def create_data():
     ]
     global answers
     answers = [6, 9, 25, 60, 2]
-
-
-def get_result(right_answers, total_answers):
+    global result
     result = [
         'Идиот',
         'Кретин',
@@ -26,56 +25,26 @@ def get_result(right_answers, total_answers):
         'Талант',
         'Гений'
     ]
-    for i in range(6):
-        if right_answers / total_answers <= (i + 1) / 6:
-            return result[i]
 
 
-def print_scoreboard():
-    file = open('game_results.txt', 'r')
-    print(f'Имя       кол-во правильных ответов   результат')
-    for line in file:
-        line_list = line.strip('\n').split('-')
-        name = line_list[0]
-        right_answers = line_list[1]
-        result = line_list[2]
-        print(f'{name:10}{right_answers:28}{result}')
-    file.close()
-
-
-def save_game_result(user_name, right_answers, result):
-    file = open('game_results.txt', 'a')
-    file.write(f'{user_name}-{right_answers}-{result}\n')
-    file.close()
-
-
-def validate_user_answer(user_input):
-    if not user_input.isdigit():
-        print('нужно ввести число')
-        return validate_user_answer(input())
-    return int(user_input)
-
-
-def init_test():
+def init_game():
     right_answers_counter = 0
     create_data()
-    total_questions = len(questions)
     print('как вас зовут?')
-    user_name = input()
+    name = input()
     for i in range(len(questions)):
         random_index = random.randint(0, len(questions) - 1)
         print(f'№{i + 1}. {questions[random_index]}')
-        user_answer = validate_user_answer(input())
+        user_answer = int(input())
         if user_answer == answers[random_index]:
             right_answers_counter += 1
         questions.pop(random_index)
         answers.pop(random_index)
-    save_game_result(user_name, right_answers_counter, get_result(right_answers_counter, total_questions))
-    print(f'Верных ответов: {right_answers_counter}. Вы, {user_name} - {get_result(right_answers_counter, total_questions)}.')
+
+    print(f'Верных ответов: {right_answers_counter}. Вы, {name} - {result[right_answers_counter]}.')
     print('хотите повторить попытку? y/n')
     if input().lower() == 'y':
-        init_test()
+        init_game()
 
 
-init_test()
-print_scoreboard()
+init_game()
